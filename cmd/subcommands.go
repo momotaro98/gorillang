f4a84d90016dc952fe485e931fe71359f2df2abe
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2020-04-19 19:22:23 +0900

    Implement CLI app

---
+++ cmd/subcommands.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"github.com/momotaro98/gorillang"
+)
+
+func NewCommandEncode() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "encode",
+		Short: "人間の言葉をゴリラ語に変換します。使い方: gorillang encode おはよう",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return errors.New("言葉を入れてください。")
+			}
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			output := gorillang.Encode(args[0])
+			fmt.Println(output)
+		},
+	}
+	return cmd
+}
+
+func NewCommandDecode() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "decode",
+		Short: "ゴリラ語を人間の言葉にします。使い方: gorillang decode ウホウォホッ！！ホッ ウホウォうほホホッ！ ウホ？ゥゥオホ！！",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			stdin := os.Stdin
+			s, _ := ioutil.ReadAll(stdin)
+			fmt.Println("stdin:", s)
+			if len(args) < 1 {
+				return errors.New("ゴリラ語を入れてください。")
+			}
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			input := strings.Join(args, " ")
+			output := gorillang.Decode(input)
+			fmt.Println(output)
+		},
+	}
+	return cmd
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/momotaro98/gorillang
 
 go 1.13
+
+require (
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/cobra v1.0.0
+	github.com/spf13/viper v1.6.3
+)
